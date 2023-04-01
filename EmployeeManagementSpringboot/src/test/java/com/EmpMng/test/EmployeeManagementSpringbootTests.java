5ce1154ec77f9e20d2f86919301da55abf6d669d
Aryan Dixit <2001aryandixit@gmail.com>
2023-04-01 15:20:06 +0530

    First commit

---
+++ EmployeeManagementSpringboot/src/test/java/com/EmpMng/test/EmployeeManagementSpringbootTests.java
@@ -0,0 +1,13 @@
+package com.EmpMng.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeManagementSpringbootTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
